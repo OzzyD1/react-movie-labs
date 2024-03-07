2951b396eb3afe986f4faff7dfde2e53f3def6f4
OzzyD1 <ozzydomarkas@gmail.com>
2024-03-07 17:18:19 +0000

    Added Upcoming movies page

--- movies/src/index.js
+++ movies/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
 
@@ -16,6 +17,10 @@ const App = () => {
                 <Route
                     path="/movies/favorites"
                     element={<FavoriteMoviesPage />}
+                />
+                <Route
+                    path="/movies/upcoming"
+                    element={<UpcomingMoviesPage />}
                 />
                 <Route path="/movies/:id" element={<MoviePage />} />
                 <Route path="/" element={<HomePage />} />
