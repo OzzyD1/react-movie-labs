54d277c4f57c5b653ed4bce4f11d8c939a5387d4
OzzyD1 <ozzydomarkas@gmail.com>
2024-03-07 18:03:31 +0000

    Exercise Two: Change upcoming movies page to use caching

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -13,6 +13,21 @@ export const getMovies = () => {
         });
 };
 
+export const getUpcomingMovies = () => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.json().message);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
 export const getMovie = (args) => {
     // console.log(args)
     const [, idPart] = args.queryKey;
@@ -68,17 +83,6 @@ export const getMovieImages = ({ queryKey }) => {
 export const getMovieReviews = (id) => {
     return fetch(
         `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-        .then((res) => res.json())
-        .then((json) => {
-            // console.log(json.results);
-            return json.results;
-        });
-};
-
-export const getUpcomingMovies = () => {
-    return fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     )
         .then((res) => res.json())
         .then((json) => {
